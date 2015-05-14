925c2611b4180f2356f69ec6991bb36457dfa58b
Kallyn Gowdy <kallyngowdy@gmail.com>
2015-05-14 01:41:33 -0400

    Add required NuGet packages for running
    - Glimpse.Server.AspNet.Sample only

--- src/Glimpse.Server.AspNet.Sample/Startup.cs
+++ src/Glimpse.Server.AspNet.Sample/Startup.cs
@@ -11,6 +11,8 @@ namespace Glimpse.Server.AspNet.Sample
         {
             services.AddGlimpse()
                     .RunningServerWeb();
+
+            services.AddMvc();
         }
 
         public void Configure(IApplicationBuilder app)
@@ -22,6 +24,18 @@ namespace Glimpse.Server.AspNet.Sample
             app.UseSignalR("/Glimpse/Data/Stream");
 
             app.UseWelcomePage();
+
+            // Add MVC to the request pipeline.
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Home", action = "Index" });
+
+                // Uncomment the following line to add a route for porting Web API 2 controllers.
+                // routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
+            });
         }
     }
 }
